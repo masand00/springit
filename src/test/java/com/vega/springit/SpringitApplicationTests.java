1ee287b158fb8540e8d673a25a160f8098708aeb
masand00 <92371725+masand00@users.noreply.github.com>
2021-10-12 13:24:32 +0200

    Initial Commit for Spring Boot Reddit Clone

---
+++ src/test/java/com/vega/springit/SpringitApplicationTests.java
@@ -0,0 +1,14 @@
+package com.vega.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
